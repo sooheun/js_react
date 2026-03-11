2921d8fb1d755f8f19b3c468b5b113f6d9b7089b
hee80 <hee8042425@gmail.com>
2026-03-11 22:11:10 +0900

    다양한 input들에 대한 예시.feat useState

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,16 +1,34 @@
 import { useState } from "react";
 
-function Body({ children })
+function Body()
 {
-  const [text, setText] = useState("");
-  const handleOnChange = (e) => {
-    setText(e.target.value);
-  };
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("");
+  const [birth, setBirth] = useState("");
+  const [bio, setBio] = useState("");
+  const onChangeName = (e) => setName(e.target.value);
+  const onChangeGender = (e) => setGender(e.target.value);
+  const onChangeBirth = (e) => setBirth(e.target.value);
+  const onChangeBio = (e) => setBio(e.target.value);
   
   return (
     <div>
-      <input value={text} onChange={handleOnChange} />
-      <div>{text}</div>
+      <div>
+        <input value={name} onChange={onChangeName} placeholder="이름" />
+      </div>
+      <div>
+        <select value={gender} onChange={onChangeGender}>
+          <option key={""}></option>
+          <option key={"남성"}>남성</option>
+          <option key={"여성"}>여성</option>
+        </select>
+      </div>
+      <div>
+        <input type="date" value={birth} onChange={onChangeBirth} />
+      </div>
+      <div>
+        <textarea value={bio} onChange={onChangeBio} />
+      </div>
     </div>
   );
 }
