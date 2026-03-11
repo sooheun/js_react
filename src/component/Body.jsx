3be0414c7aa25665d4ed693af4c333a26fb534c7
hee80 <hee8042425@gmail.com>
2026-03-11 22:28:24 +0900

    State 값 Props 전달

--- src/component/Body.jsx
+++ src/component/Body.jsx
@@ -1,39 +1,28 @@
+import "./Body.css";
 import { useState } from "react";
+
+function Viewer({ number }) {
+  return <div>{number % 2 === 0 ? <h3>짝수</h3> : <h3>홀수</h3>}</div>;
+}
 
 function Body()
 {
-  const [state, setState] = useState({
-    name: "",
-    gender: "",
-    birth: "",
-    bio: "",
-  });
-  const handleOnChange = (e) => {
-    console.log("현재 수정 대상:", e.target.name);
-    console.log("수정값:", e.target.value);
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
+  const [number, setNumber] = useState(0);
+  const onIncrease = () => {
+    setNumber(number + 1);
+  };
+  const onDecrease = () => {
+    setNumber(number - 1);
   };
   
   return (
     <div>
+      <h2>{number}</h2>
+      <Viewer number={number} />
+
       <div>
-        <input name="name"value={state.name} onChange={handleOnChange} placeholder="이름" />
-      </div>
-      <div>
-        <select name="gender" value={state.gender} onChange={handleOnChange}>
-          <option key={""}></option>
-          <option key={"남성"}>남성</option>
-          <option key={"여성"}>여성</option>
-        </select>
-      </div>
-      <div>
-        <input name="birth" type="date" value={state.birth} onChange={handleOnChange} />
-      </div>
-      <div>
-        <textarea name="bio" value={state.bio} onChange={handleOnChange} />
+        <button onClick={onIncrease}>+</button>
+        <button onClick={onDecrease}>-</button>
       </div>
     </div>
   );
