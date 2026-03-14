c559852d516539160ddf297559c6bed6ad32ed69
hee80 <hee8042425@gmail.com>
2026-03-14 21:49:47 +0900

    useEffect 예시

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import  Viewer from "./component/Viewer";
 import Controller from './component/Controller';
 
 function App()
 {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
   const handleSetCount = (value) => {
     setCount(count + value);
   };
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  useEffect(() => {
+    console.log("업데이트: ", text, count);
+  }, [count, text]);
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
+      <section>
+        <input value={text} onChange={handleChangeText} />
+      </section>
       <section>
         <Viewer count={count} />
       </section>
